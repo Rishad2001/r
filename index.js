b9695e0ef838a431dc248cad52dde6a77e2c0de8
unknown <r23richu@gmail.com>
2021-06-25 23:42:04 +0530

    refreshing the page

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 const taskContainer = document.querySelector(".task__container");
 console.log(taskContainer);
 
+//global store
+const globalStore = [];
+
 const newCard = ({
     id, 
     imageUrl, 
@@ -31,6 +34,22 @@ const newCard = ({
 </div>`;
 
 
+//for getting the things in local storage after refreshing
+const loadInitialTaskCards = () =>{
+  //access local storage
+  const getInitialData = localStorage.getItem("tasky");
+  if (!getInitialData) return;
+  //convert stringified-object to object
+  const { cards } = JSON.parse(getInitialData); 
+  //map arround the array to generate HTML card and inject it to DOM
+  cards.map((cardObject) => {
+    const createNewCard =newCard(cardObject);
+    taskContainer.insertAdjacentHTML("beforeend", createNewCard);
+    globalStore.push(cardObject);  
+  });
+}; 
+
+
 const saveChanges= () => {
     const taskData = {
         id: `${Date.now()} `, // unique number for card id
@@ -39,10 +58,17 @@ const saveChanges= () => {
         taskType: document.getElementById("tasktype").value  ,
         taskDescription: document.getElementById("taskdescription").value  ,
     };
+    //html code
      const createNewCard =newCard(taskData);
 
      taskContainer.insertAdjacentHTML("beforeend", createNewCard);
-};
+     globalStore.push(taskData);
+     // for storing to local storage 
+     //we cannot directly store array in local stporage thats why we store it in object
+     //here tasky is the key & the object {cards: globalStore is our data}
+     //JSON.stringify for converting object to string
+     localStorage.setItem("tasky", JSON.stringify({ cards: globalStore }));
+  };
 
 // write about modal 
 //write about onclick
